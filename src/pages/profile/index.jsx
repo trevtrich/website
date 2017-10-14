4318f940b4e33328b902cd92aa4e38190f653b18
Trevor Richardson <trevor@gaincompliance.com>
2017-10-14 09:32:36 -0500

    Added degrees to education

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -172,88 +172,82 @@ class Profile extends React.Component {
             //          </div>
             //        </section>
         }
-        <section className="text-center" id="concept" >
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h2 className="section-heading">WORK</h2>
-                <hr className="border-primary" />
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="row">
-              <div
-                className="col-md-6 wow slideInLeft"
-                data-wow-duration="1.0s"
-              >
-                <img
-                  src={pathPrefix + '/img/work1.png'}
-                  alt="work1"
-                  className="rounded-circle mx-auto"
-                />
-                <p>Yomu</p>
-              </div>
-              <div
-                className="col-md-6 wow slideInRight"
-                data-wow-duration="1.0s"
-              >
-                <img
-                  src={pathPrefix + '/img/work2.png'}
-                  alt="work2"
-                  className="rounded-circle mx-auto"
-                />
-                <p>Detector</p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section id="repos">
+        {
+            //            <section className="text-center" id="concept" >
+            //          <div className="container">
+            //            <div className="row">
+            //              <div className="col-lg-12">
+            //                <h2 className="section-heading">WORK</h2>
+            //                <hr className="border-primary" />
+            //              </div>
+            //            </div>
+            //          </div>
+            //          <div className="container">
+            //            <div className="row">
+            //              <div
+            //                className="col-md-6 wow slideInLeft"
+            //                data-wow-duration="1.0s"
+            //              >
+            //                <img
+            //                  src={pathPrefix + '/img/work1.png'}
+            //                  alt="work1"
+            //                  className="rounded-circle mx-auto"
+            //                />
+            //                <p>Yomu</p>
+            //              </div>
+            //              <div
+            //                className="col-md-6 wow slideInRight"
+            //                data-wow-duration="1.0s"
+            //              >
+            //                <img
+            //                  src={pathPrefix + '/img/work2.png'}
+            //                  alt="work2"
+            //                  className="rounded-circle mx-auto"
+            //                />
+            //                <p>Detector</p>
+            //              </div>
+            //            </div>
+            //          </div>
+            //        </section>
+        }
+        {
+            //        <section id="repos">
+            //          <div className="container">
+            //            <div className="row align-items-center">
+            //              <div className="col-md-6 text-center">
+            //                <h2 className="section-heading">Repositories</h2>
+            //              </div>
+            //              <div className="col-md-6 text-left">
+            //                <li>
+            //                  <a href="https://github.com/jaxx2104/gatsby-starter-bootstrap">
+            //                    gatstrap
+            //                  </a>
+            //                </li>
+            //                <li>
+            //                  <a href="https://github.com/jaxx2104/gulp-sample">
+            //                    gulp-sample
+            //                  </a>
+            //                </li>
+            //                <li>
+            //                  <a href="https://github.com/jaxx2104/irm">irm</a>
+            //                </li>
+            //                <li>
+            //                  <a href="https://github.com/jaxx2104/mt-dataapi-php">
+            //                    mt-dataapi-php
+            //                  </a>
+            //                </li>
+            //              </div>
+            //            </div>
+            //          </div>
+            //        </section>
+        }
+        <section id="education">
           <div className="container">
             <div className="row align-items-center">
-              <div className="col-md-6 text-center">
-                <h2 className="section-heading">Repositories</h2>
-              </div>
-              <div className="col-md-6 text-left">
-                <li>
-                  <a href="https://github.com/jaxx2104/gatsby-starter-bootstrap">
-                    gatstrap
-                  </a>
-                </li>
-                <li>
-                  <a href="https://github.com/jaxx2104/gulp-sample">
-                    gulp-sample
-                  </a>
-                </li>
-                <li>
-                  <a href="https://github.com/jaxx2104/irm">irm</a>
-                </li>
-                <li>
-                  <a href="https://github.com/jaxx2104/mt-dataapi-php">
-                    mt-dataapi-php
-                  </a>
-                </li>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section id="features">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-md-6">
-                <img
-                  src={pathPrefix + '/img/work3.png'}
-                  className="img-rounded img-responsive"
-                  title=""
-                  alt=""
-                />
-              </div>
-              <div className="col-md-6 text-center align-middle">
+              <div>
                 <h2 className="section-heading">Education</h2>
-                <p>
-                    Add something here!
-                </p>
+                <p>B.S. Mechanical Engineering, Iowa State University, 3.72 GPA</p>
+                <p>M.S. Co-Major Human Computer Interaction & Computer Engineering, Iowa State University, 3.94 GPA</p>
               </div>
             </div>
           </div>
