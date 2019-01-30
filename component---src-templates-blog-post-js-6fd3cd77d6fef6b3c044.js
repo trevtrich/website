b20d3a8a4bf9970335e1cc5a0c419febc72629be
Trevor Richardson <trevor@gaincompliance.com>
2019-01-30 16:05:40 -0600

    Updates

--- component---src-templates-blog-post-js-6fd3cd77d6fef6b3c044.js
+++ component---src-templates-blog-post-js-6fd3cd77d6fef6b3c044.js
@@ -3506,7 +3506,7 @@ webpackJsonp([107818501498521],{
 
 /***/ }),
 
-/***/ 610:
+/***/ 612:
 /***/ (function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_RESULT__;'use strict';
@@ -3840,7 +3840,7 @@ webpackJsonp([107818501498521],{
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	__webpack_require__(495);
+	__webpack_require__(497);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
@@ -3872,7 +3872,7 @@ webpackJsonp([107818501498521],{
 
 /***/ }),
 
-/***/ 495:
+/***/ 497:
 /***/ (function(module, exports) {
 
 	// empty (null-loader)
@@ -4061,7 +4061,7 @@ webpackJsonp([107818501498521],{
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var _striptags = __webpack_require__(610);
+	var _striptags = __webpack_require__(612);
 	
 	var _striptags2 = _interopRequireDefault(_striptags);
 	
